f5074d9029fb267534dca9df10173d693cb9faaf
aaronszt <aronmaster94@gmail.com>
2023-09-13 20:30:38 -0300

    this is a commit for delete my coments

--- src/app.js
+++ src/app.js
@@ -3,15 +3,13 @@ const morgan = require('morgan');
 require('dotenv').config();
 const { URL } = process.env;
 const routes = require('./routes/index.js');
-//const expressValidator = require('../src/middleware'); aca es como se veria una carpeta con el middleware de validacion con express-validator
 
 const server = express();
 
-//server.use(expressValidator()); aca llamaria a la funcion de validaciones 
 server.use(express.json());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', `${URL}`); // aca no se que ponerle xd porque no seria localhost:3000 3===D
+    res.header('Access-Control-Allow-Origin', `${URL}`);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
@@ -29,6 +27,3 @@ server.use((err, req, res, next) => {
 });
 
 module.exports = server;
-
-
-// se hizo esta configuracion si esta mal vallase a la concha de su madre xd <3
