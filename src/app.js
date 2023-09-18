6998cb7b7396ccb022c21edaab678326bd7da498
Federico <gabi.bastias10@gmail.com>
2023-09-18 13:47:55 -0300

    Solved some .env variable pre deploy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const handlerError = require('./handler/handlerError');
 const cookieParser = require('cookie-parser')
+const { URL } = process.env;
 
 const server = express();
 
@@ -11,7 +12,7 @@ server.use(express.json());
 server.use(morgan('dev'));
 server.use(cookieParser())
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+        res.header('Access-Control-Allow-Origin', `${URL}`)
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,24 +3,24 @@ const { validateToken, createToken } = require('../services/token');
 const { Users } = require('../models/Users');
 const { transporter, mailContent } = require('../services/nodemailer');
 const { handlerForgotPass, handlerResetPass } = require('../handler/handleUserDB');
-const { ENTRY, PORT } = process.env
+const { URL } = process.env
 
 //ruta para validar el token.
 function requireAuthController(req,res){
     try {
         let token;
-    if(req.cookies.jwt){
-        token = req.cookies.jwt
-    }else if(req.body.token){
-        token = req.body.token;
-    } else return res.status(400).json({access:false})
-    
-    if(token){
-        const validate = validateToken(token)
-        if(!validate?.error) return res.status(200).json({access:true})
-        else return res.status(400).json({acess:false, expirate:validate.error})
-    } 
-     throw Error('token is invalid')
+        if(req.cookies.jwt){
+            token = req.cookies.jwt
+        }else if(req.body.token){
+            token = req.body.token;
+        } else return res.status(400).json({access:false})
+        
+        if(token){
+            const validate = validateToken(token)
+            if(!validate?.error) return res.status(200).json({access:true})
+            else return res.status(400).json({acess:false, expirate:validate.error})
+        } 
+        throw Error('token is invalid')
     } catch (error) {
         res.status(400).json({access:false, message:error.message})
     }
@@ -33,7 +33,7 @@ async function forgotPassword(req, res){
             const userExist = await handlerForgotPass(Users, email);
             if (userExist) {
                 const token = createToken(userExist._id);
-                const link = `${ENTRY}:${PORT}/auth/reset-password/${token}`;
+                const link = `${URL}/auth/reset-password/${token}`;
                 transporter.sendMail(mailContent(userExist.email, link), (error, info) => {
                     if (error) {
                         throw new Error('Error sending email');
