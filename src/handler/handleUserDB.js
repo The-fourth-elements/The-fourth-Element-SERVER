b001a31f153e64d7e023a7e378ba9800ec1161df
Federico <gabi.bastias10@gmail.com>
2023-09-25 12:38:13 -0300

    Logueo y creacion de usuario cambiado

--- src/handler/handleUserDB.js
+++ src/handler/handleUserDB.js
@@ -3,38 +3,43 @@ const { encrypt } = require('../services/crypt');
 const { decriptToken } = require('../services/token');
 const { Users } = require('../models/Users')
 
-// async function handlerForgotPass(email){
-//     try {
-//         const userEmail = await Users.findOne({email: email});
-//         if (!email || !isEmail(email)) {
-//             throw Error('Not a valid Email');
-//         } else {
-//             if (userEmail) {
-//                 return userEmail;
-//             } else {
-//                 throw new Error("Didn't exist an account with this e-mail.");
-//             }
-//         }
-//     } catch (error) {
-//         return { error: error.message }
-//     }
-// }
+async function handlerForgotPass(email){
+    try {
+        const userEmail = await Users.findOne({email: email});
+        if (!email || !isEmail(email)) {
+            throw Error('Not a valid Email');
+        } else {
+            if (userEmail) {
+                return userEmail;
+            } else {
+                throw new Error("Didn't exist an account with this e-mail.");
+            }
+        }
+    } catch (error) {
+        return { error: error.message }
+    }
+}
 
-// async function handlerResetPass(token, password){
-//     try {
-//         const userToken = decriptToken(token);
-//         const matchUser = await Users.findById(userToken.data);
-//         if (matchUser) {
-//             if (password) {
-//                 const newPass = await encrypt(password);
-//                 await matchUser.updateOne({password: newPass});
-//             }else {
-//                 throw new Error('Not matching.');
-//             }
-//         } else {
-//             throw new Error ('No data found for the given ID.')
-//         }
-//     } catch (error) {
-//         return { error: error.message }
-//     }
-// }
+async function handlerResetPass(token, password){
+    try {
+        const userToken = decriptToken(token);
+        const matchUser = await Users.findById(userToken.data);
+        if (matchUser) {
+            if (password) {
+                const newPass = await encrypt(password);
+                await matchUser.updateOne({password: newPass});
+            }else {
+                throw new Error('Not matching.');
+            }
+        } else {
+            throw new Error ('No data found for the given ID.')
+        }
+    } catch (error) {
+        return { error: error.message }
+    }
+}
+
+module.exports = {
+    handlerForgotPass,
+    handlerResetPass
+}
