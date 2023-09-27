927b9689d4bf9ca7058ee4cd45aa9b6062fae687
Federico <gabi.bastias10@gmail.com>
2023-09-27 00:10:15 -0300

    Email cambiado

--- src/controllers/getUserByEmail.js
+++ src/controllers/getUserByEmail.js
@@ -5,9 +5,8 @@ async function getUserByMail(req, res, next){
         const { email } = req.body;
         if(!email) throw Error('Missing email');
         const user = await Users.findOne({email});
-        console.log(user);
         if(!user) throw Error('Not matching email');
-        return res.status(200).json(user);
+        return res.status(200).json({user: true});
     } catch (error) {
         return res.status(404).json({error: error.message});
     }
