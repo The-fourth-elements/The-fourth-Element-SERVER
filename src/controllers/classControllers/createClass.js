31f643697454775daaee621def378f6f8ed5ca2a
EmilioFlCar <129438622+EmilioFlCar@users.noreply.github.com>
2023-09-28 10:01:12 -0500

    Update createClass.js
    minor changes

--- src/controllers/classControllers/createClass.js
+++ src/controllers/classControllers/createClass.js
@@ -1,11 +1,19 @@
 const Class = require("../../models/Class");
 const Video = require('../../models/Videos')
 const PowerPoint = require('../../models/PowerPoint')
+const mongoose = require('mongoose');
 
 async function createClass(req, res, next) {
+    let session = null
     try {
         const { name, description, video, powerPoint } = req.body
-        if (!name || !description) throw Error("Faltan datos")
+        if (!name || !description) {
+            const error = new Error("Faltan datos")
+            error.statusCode = 400
+            throw error}
+
+        session = await mongoose.startSession();
+        session.startTransaction();
 
         let videoId, powerPointId;
 
@@ -16,16 +24,23 @@ async function createClass(req, res, next) {
             })
             await newVideo.save()
             videoId = newVideo._id
-        } else throw('Falta el video.')
+        } else {
+            const error = new Error('Falta el video.')
+            error.statusCode = 400
+            throw error
+        }
 
         if (powerPoint) {
             const newPresentacion = new PowerPoint({
-                id: powerPoint.id,
                 url: powerPoint.url
             })
             await newPresentacion.save()
             powerPointId = newPresentacion._id
-        } else throw('Falta el Power Point.')
+        } else {
+            const error = new Error('Falta el Power Point.')
+            error.statusCode = 400
+            throw error
+        }
 
         const newClass = new Class({
             name,
@@ -35,9 +50,16 @@ async function createClass(req, res, next) {
         })
         await newClass.save()
 
+        await session.commitTransaction();
+        session.endSession()
+
         if (newClass) res.status(201).json(newClass)
     } catch (error) {
-        next({ message: error.message, statusCode: 204 });
+        if (session) {
+            await session.abortTransaction();
+            session.endSession();
+        }
+        next({ message: error.message, statusCode: error.statusCode });
     }
 }
 
