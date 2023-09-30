08b465c1abc3f1d2ef10fe417e27a1f10c6a6320
EmilioFlCar <129438622+EmilioFlCar@users.noreply.github.com>
2023-09-30 16:35:20 -0500

    firebase feature deletion

--- src/services/firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-
-const firebase = admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-});
-
-module.exports = {
-    auth: firebase.auth()
-};
