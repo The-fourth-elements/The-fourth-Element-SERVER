178c01ab34cbb5b9883dc64203dae26323b297d2
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-10-12 18:56:40 -0300

    Arreglos varios

--- src/controllers/paymentGateway/reciveWebhook.js
+++ src/controllers/paymentGateway/reciveWebhook.js
@@ -1,5 +1,4 @@
 const mercadopago = require('mercadopago');
-const Users = require('../../models/Users');
 
 async function reciveWebhook(req, res, next) {
     try {
@@ -9,7 +8,6 @@ async function reciveWebhook(req, res, next) {
             const data = await mercadopago.payment.findById(payment["data.id"]);
             const response = data.body.status;
             if(response === "aproved"){
-                // const newRol = await Users.findByIdAndUpdate(id, { role: 1 });
                 res.status(200).send('Success.');
             } else if(response === "rejected"){
                 throw Error("El pago fue rechazado.");
