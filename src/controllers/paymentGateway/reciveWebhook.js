c9df86f3023e01f033b4d0c97eba84e97dde5d4b
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-10-12 18:42:14 -0300

    Cambio de rol al pagar el curso

--- src/controllers/paymentGateway/reciveWebhook.js
+++ src/controllers/paymentGateway/reciveWebhook.js
@@ -9,6 +9,7 @@ async function reciveWebhook(req, res, next) {
             const data = await mercadopago.payment.findById(payment["data.id"]);
             const response = data.body.status;
             if(response === "aproved"){
+                // const newRol = await Users.findByIdAndUpdate(id, { role: 1 });
                 res.status(200).send('Success.');
             } else if(response === "rejected"){
                 throw Error("El pago fue rechazado.");
