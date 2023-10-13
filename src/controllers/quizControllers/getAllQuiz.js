10ec153a3c71bd27f3ec2955b1ca97b0deac5f2b
aaronszt <aronmaster94@gmail.com>
2023-10-12 21:45:21 -0300

    fix getAllQuiz, getOneQuiz

--- src/controllers/quizControllers/getAllQuiz.js
+++ src/controllers/quizControllers/getAllQuiz.js
@@ -2,7 +2,7 @@ const Quiz = require('../../models/Quiz');
 
 async function getAllQuiz (req, res, next){
     try {
-        const quiz = await Quiz.find({}).populate("quest").populate("results");
+        const quiz = await Quiz.find({});
         if(!quiz) throw Error('No se encontraron quiz');
         return res.status(200).json(quiz);
     } catch (error) {
