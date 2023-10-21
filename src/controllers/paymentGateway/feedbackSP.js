f7ee349d292899bb0a4488ab2e0b72756790b76f
NONHIGH <angelones_24@hotmail.com>
2023-10-21 18:27:47 -0300

    fix feedback sp

--- src/controllers/paymentGateway/feedbackSP.js
+++ src/controllers/paymentGateway/feedbackSP.js
@@ -2,26 +2,25 @@ require('dotenv').config();
 const { URL, STRIPE_SECRET_KEY } = process.env;
 const { Stripe } = require('stripe');
 const { Users } = require('../../models/Users');
+const { payment } = require('mercadopago');
 
 const feedbackSP = async (request, response) => {
     try {
         const { userId } = request.query;
-        // if (!userId) throw Error('No se encontro el id del usuario');
-        //logica para cambiar el usuario por id
         const stripe = new Stripe(STRIPE_SECRET_KEY);
+        const user = await Users.findById(userId);
+        const cs = await stripe.checkout.sessions.retrieve(user.stripe_payment);
 
-        const userFound = await Users.findByIdAndUpdate(userId, { role: 1 }, { new: true });
-        // const k = await stripe.checkout.sessions.retrieve(userFound.stripe_payment);
-        // console.log(k, 'soy el cargo')
-        if (userFound.stripe_payment) {
-            //logica para cambiar el usuario
-            console.log(userFound);
-            console.log('he entrado aqui pendejo');
+
+        if (cs.payment_status === 'paid') {
+            user.stripe_payment = 0
+            user.role = 1;
+            await user.save();
+            return response.redirect(`${URL}/paid-success`)
         }
 
-        console.log(request.query);
-        response.redirect(`${URL}/paid-success`)
-        // return response.status(200).json({ 'message': 'holis' })
+        throw new Error('No se ha pagado el curso aun')
+
 
     } catch (error) {
         return response.status(400).json({ error: error.message })
