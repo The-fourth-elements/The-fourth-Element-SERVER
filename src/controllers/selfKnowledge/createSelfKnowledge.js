49d72d51008c227f00c44d347c8dd8b648fed85b
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-10-26 20:48:52 -0300

    Ultimops cambios

--- src/controllers/selfKnowledge/createSelfKnowledge.js
+++ src/controllers/selfKnowledge/createSelfKnowledge.js
@@ -3,19 +3,15 @@ const Module = require("../../models/Module");
 
 
 async function createSelfKnowledge(req, res, next){
-    
-    // console.log(selfKnowledge);
+    const { moduleId } = req.params;
+    const { selfKnowledge } = req.body;
     try {
-        const { moduleId } = req.params;
-        const { selfKnowledge } = req.body; 
         if(!selfKnowledge) throw Error('Faltan datos');
         const newSelfKnowledge = await Promise.all(selfKnowledge.map(async selk => {
-            // console.log(selk);
-            // return selk
             const newSelfk = await findOrCreateSelfKnowledge(selk);
             return newSelfk;
         }));
-        // if(newSelfKnowledge.length <= 0) throw Error('No se pudo crear el Autoconocimiento');
+        if(newSelfKnowledge.length <= 0) throw Error('No se pudo crear el Autoconocimiento');
         const findModule = await Module.findById(moduleId);
         if (!findModule) throw Error('No se encontro el módulo al que agregar el autoconocimiento.');
         findModule.selfKnowledge = newSelfKnowledge;
--- src/controllers/usersControllers/orderUsersByAZ.js
+++ src/controllers/usersControllers/orderUsersByAZ.js
@@ -1,17 +1,16 @@
 const { Users } = require('../../models/Users');
 
 async function orderUsersByAZ (req, res, next){
-    console.log(req.query.hasOwnProperty('ASC'));
+    console.log(req.query);
     try {
         if (req.query.hasOwnProperty('ASC') || req.query.hasOwnProperty('DSC')) {
-            const { ASC, DSC } = req.query;
             const users = {};
             switch (req.query) {
-                case ASC:
+                case {ASC: true}:
                     users = await Users.find({}).sort({username: 1});
                     if (users.length <= 0) throw Error;
                     else res.status(200).json(users);
-                case DSC:
+                case {DSC: true}:
                     users = await Users.find({}).sort({username: -1});
                     if (users.length <= 0) throw Error;
                     else res.status(200).json(users);
