612e3777053ae20c726f5553c492867b230f3065
Vale51 <102689997+Vale51@users.noreply.github.com>
2023-11-01 16:14:26 -0300

    faltaba sacar localStorage

--- src/middlewares/roleToModule.js
+++ src/middlewares/roleToModule.js
@@ -2,14 +2,14 @@ const Module = require("../models/Module");
 const { Users } = require("../models/Users");
 
 async function roleToModule(req, res, next){
-    const { userId, moduleId } = req.body
+    const { jsdklfsdjklfdsjfds, moduleId } = req.cookies
     try {
-        if (!userId || ! moduleId) throw Error('Faltan datos');
-        const userRole = (await Users.findById(userId)).role;
+        if (!jsdklfsdjklfdsjfds || ! moduleId) throw Error('Faltan datos');
+        const userRole = (await Users.findById(jsdklfsdjklfdsjfds)).role;
         const modulePaid = (await Module.findById(moduleId)).paid;
-        if (userRole >= 1 || modulePaid === false) return res.status(200).send('Aprovado');
+        if (userRole >= 1 || modulePaid === false) return res.status(200).json({message : 'Aprobado'});
         else if(userRole <= 0 && modulePaid === true) throw Error('No puede ingresar porque es un módulo pago.');
-        else res.status(200).send('Aprovado');
+        else res.status(200).json({message : 'Aprobado'});
     } catch (error) {
         next({ message: error.message, statusCode: 404});
     }
