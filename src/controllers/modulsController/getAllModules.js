af049560cb69aae4869427203fe27bcc9cb09f7b
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-11-13 17:03:42 -0300

    Update getAllModules.js

--- src/controllers/modulsController/getAllModules.js
+++ src/controllers/modulsController/getAllModules.js
@@ -12,12 +12,7 @@ async function getAllModules(req, res, next){
             .populate('selfRegister')
 
         if (modules.length <= 0) throw Error('No se encontraron módulos.')
-        else {
-            const roleUser = await roleMiddleware(jsdklfsdjklfdsjfds);
-            if (roleUser >= 1) {
-                res.status(200).json(modules)
-            } else throw Error('No puede acceder al módulo por su rol');
-        }
+        else return res.status(200).json(modules);
     } catch (error) {
         next({ message: error.message, statusCode: 404});
     }
