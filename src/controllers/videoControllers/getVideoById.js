853e8fba845157467e29004b990697859a720779
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-11-15 19:14:33 -0300

    Signature a videos

--- src/controllers/videoControllers/getVideoById.js
+++ src/controllers/videoControllers/getVideoById.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const cloudinary = require('../../utils/cloudinary');
+const { CLOUDINARY_API_SECRET } = process.env;
 const Videos = require('../../models/Videos');
 
 async function getVideoById(req, res, next) {
@@ -5,9 +8,15 @@ async function getVideoById(req, res, next) {
         const { id } = req.params
         const video = await Videos.findById(id);
 
+        const signature = cloudinary.utils.api_sign_request({id: video.id}, CLOUDINARY_API_SECRET);
+        const secureURL = cloudinary.url(video.id, {
+            secure: true,
+            signature: signature
+        });
+
         if (!video) res.status(404).json({ message: 'Video no encontrado' });
 
-        res.status(200).json(video);
+        res.status(200).json({url: secureURL});
 
     } catch (error) {
         next({ message: error.message, statusCode: 404 });
