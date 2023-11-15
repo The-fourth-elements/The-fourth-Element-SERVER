da0baebc04f8c1630d487629ff12f8c5c8f6847b
Federico Gabriel Bastias Cano <38295529+GabiBastias@users.noreply.github.com>
2023-11-15 19:51:56 -0300

    Cambiado getVIdeo

--- src/controllers/videoControllers/getVideoById.js
+++ src/controllers/videoControllers/getVideoById.js
@@ -7,17 +7,9 @@ async function getVideoById(req, res, next) {
     try {
         const { id } = req.params
         const video = await Videos.findById(id);
-
-        const signature = cloudinary.utils.api_sign_request({id: video.id}, CLOUDINARY_API_SECRET);
-        const secureURL = cloudinary.url(video.id, {
-            secure: true,
-            signature: signature
-        });
-
         if (!video) res.status(404).json({ message: 'Video no encontrado' });
-
-        res.status(200).json({url: secureURL});
-
+        const signature = cloudinary.utils.api_sign_request({url: video.url}, CLOUDINARY_API_SECRET);
+        return res.status(200).json({url: `${video.url}?signature=${signature}`});
     } catch (error) {
         next({ message: error.message, statusCode: 404 });
     }
